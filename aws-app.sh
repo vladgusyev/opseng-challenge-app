82d1d3ee01f986a01b36024500bf0669f2ef2ff7
EC2 Default User <ec2-user@ip-172-31-42-147.us-west-2.compute.internal>
2017-02-10 03:56:53 +0000

    aws-app.sh

--- aws-app.sh
+++ aws-app.sh
@@ -12,19 +12,23 @@ printf "Visit the following URL: http://$EXTERNAL_IP \n"
 }
 
 STARTAPP(){
-/usr/bin/virtualenv pvenv
-source "$ROOT_DIR/$PYTHON_VENV/bin/activate" 
-$ROOT_DIR/$PYTHON_VENV/bin/easy_install gunicorn flask
-$ROOT_DIR/$PYTHON_VENV/bin/python $ROOT_DIR/$APP_NAME &>/dev/null
-$ROOT_DIR/$PYTHON_VENV/bin/gunicorn app:app &>/dev/null
+/usr/bin/virtualenv pvenv &>/dev/null
+source "$ROOT_DIR/$PYTHON_VENV/bin/activate" &>/dev/null
+$ROOT_DIR/$PYTHON_VENV/bin/easy_install gunicorn flask &> $ROOT_DIR/easy_install.log &
+$ROOT_DIR/$PYTHON_VENV/bin/gunicorn app:app &> $ROOT_DIR/gunicorn.log &
 }
 
 
 if [[ -f $PIP_PATH ]];then
 	if [[ $APP_FILE_DEP -eq 0 ]];then
 		case $VIRTUALENV_DEP in
-			0 ) echo "APP_FILE_DEP and VIRTUALENV_DEP are good, will continue";STARTAPP;;
-			1 ) echo "VIRTUALENV_DEP is status 1";;
+			0 ) STARTAPP;
+				ECHOURL;
+				;;
+			1 ) /usr/bin/virtualenv pvenv &>/dev/null;
+				STARTAPP;
+				ECHOURL;
+				;;
 			* ) printf "Failed to get virtualenv dependency status";exit;;
 		esac
 	else
@@ -33,16 +37,3 @@ if [[ -f $PIP_PATH ]];then
 else
 	printf "Could not locate $PIP_PATH, cannot continue";exit;
 fi
-
-ECHOURL
-
-
-
-
-#/usr/bin/pip install virtualenv
-#/usr/bin/virtualenv pvenv
-#source pvenv/bin/activate
-#pvenv/bin/easy_install gunicorn flask
-#python app.py
-#gunicorn app:app
-#dig +short myip.opendns.com @resolver1.opendns.com.
